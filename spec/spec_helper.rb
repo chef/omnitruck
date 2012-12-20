22d814ca75f1927fae5d053cc7e72d2b8cce0973
Christopher Maier <cm@opscode.com>
2012-12-20 14:38:13 -0500

    Add real server data, plus RSpec niceties
    The server JSON was taken from data that was live at the time.  I've added several additional versions to the Ubuntu 10.04 x86_64 section in anticipation of testing the handling of semantic version information.
    
    Also added some RSpec tweaks to allow filtering (using :focus), as well as easily finding JSON data files in the tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,28 @@ Sinatra::Base.set :aws_access_key_id, ''
 Sinatra::Base.set :aws_secret_access_key, ''
 Sinatra::Base.set :aws_bucket, 'opscode-omnitruck-test'
 
+module Opscode
+  module Omnitruck
+    module RSpec
+
+      SPEC_DATA = File.expand_path(File.join(File.dirname(__FILE__), "data"))
+      
+      # Returns the contents of the given server data JSON file as a string
+      def server_data(name)
+        File.join(SPEC_DATA, "server_data", "#{name}.json")
+      end
+
+    end
+  end
+end
+
 RSpec.configure do |conf|
+  conf.include Opscode::Omnitruck::RSpec
   conf.include Rack::Test::Methods
+  
+  conf.filter_run :focus => true
+  conf.run_all_when_everything_filtered = true
+  conf.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
 
