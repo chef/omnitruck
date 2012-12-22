1ed25211638e45ba035ae4a4c55b7236c8bae20b
Christopher Maier <cm@opscode.com>
2012-12-21 22:15:12 -0500

    Add Rubygems Version

---
+++ lib/opscode/rubygems_version.rb
@@ -0,0 +1,26 @@
+require 'opscode/version'
+
+module Opscode
+  class RubygemsVersion < Version
+
+    OPSCODE_RUBYGEMS_REGEX = /^(\d+)\.(\d+)\.(\d+)(?:\.((?:alpha|beta|rc)\.\d+))?(?:\-(\d+))?$/
+
+    def initialize(version)
+      match = version.match(OPSCODE_RUBYGEMS_REGEX)
+      raise ArgumentError, "'#{version}' is not a valid Opscode Rubygems version string!" unless match
+      
+      @input = version
+      
+      @major, @minor, @patch, @prerelease, @build = match[1..5]
+      @major, @minor, @patch = [@major, @minor, @patch].map(&:to_i)
+      
+      # Do not convert @build to an integer; SemVer sorting logic will handle the conversion
+      @prerelease = nil if (@prerelease.nil? || @prerelease.empty?)
+    end
+
+    def to_s
+      @input
+    end
+
+  end
+end
---
+++ spec/rubygems_version_spec.rb
@@ -0,0 +1,32 @@
+require 'opscode/rubygems_version'
+
+describe Opscode::RubygemsVersion do
+  context "#initialize" do    
+    versions = [
+                ["10.1.1",         [10,1,1, nil, nil]],
+                ["10.1.1.alpha.1", [10,1,1, "alpha.1", nil]],
+                ["10.1.1.alpha.2", [10,1,1, "alpha.2", nil]],
+                ["10.1.1.beta.1",  [10,1,1, "beta.1", nil]],
+                ["10.14.4-1",      [10, 14, 4, nil, "1"]],
+                ["10.14.4-2",      [10, 14, 4, nil, "2"]],
+                ["10.16.0-1",      [10, 16, 0, nil, "1"]],
+                ["10.16.0.rc.0-1", [10, 16, 0, "rc.0", "1"]],
+                ["10.16.0.rc.1-1", [10, 16, 0, "rc.1", "1"]],
+                ["10.16.2-1",      [10, 16, 2, nil, "1"]]
+               ]
+
+    versions.each do |input|
+      version_string, pieces = input
+      major, minor, patch, prerelease, build = pieces
+
+      it "works for #{version_string}" do
+        v = Opscode::RubygemsVersion.new(version_string)
+        v.major.should eq major
+        v.minor.should eq minor
+        v.patch.should eq patch
+        v.prerelease.should eq prerelease
+        v.build.should eq build
+      end
+    end
+  end
+end
