8873557ad3c2886cbc6ebf078375d3cefc05a2f2
Seth Chisamore <schisamo@opscode.com>
2012-12-26 17:04:51 -0500

    update existing app_spec for expanded test fixtures
    The new set of fixtures are derived from 'real' release JSON.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -17,24 +17,9 @@ describe 'Omnitruck' do
   end
 
   describe "/download" do
-    before do
-      build_list_json = { 'run_data' => { 'timestamp' => 'Thu Aug 16 11:48:08 -0700 2012' }, 
-                          'el' => { '5' => { 'x86_64' => { '10.12.0-1' => '/el/5/x86_64/chef-10.12.0-1.el5.x86_64.rpm',
-                                                           '10.14.8-1' => '/el/5/x86_64/chef-10.14.8-1.el5.x86_64.rpm',
-                                                           '10.14.10.rc.0-1' => '/el/5/x86_64/chef-10.14.10.rc.0-1.el5.x86_64.rpm',
-                                                           '10,version-regex-is-fun' => '/el/5/x86_64/chef-10,version-regex-is-fun.el5.x86_64.rpm' },
-                                             'i686' => { '10.12.0-1' => '/el/5/x86_64/chef-10.12.0-1.el5.i686.rpm',
-                                                         '10.14.8-1' => '/el/5/x86_64/chef-10.14.8-1.el5.i686.rpm',
-                                                         '10.14.10.rc.0-1' => '/el/5/x86_64/chef-10.14.10.rc.0-1.el5.i686.rpm' },
-                                           },
-                                    '6' => { 'x86_64' => { '10.12.0-1' => '/el/6/x86_64/chef-10.12.0-1.el6.x86_64.rpm',
-                                                           '10.12.0-10' => '/el/6/x86_64/chef-10.12.0-10.el6.x86_64.rpm' },
-                                             'i686' => { '10.12.0-1' => '/el/6/i686/chef-10.12.0-1.el6.i686.rpm',
-                                                         '10.12.0-10' => '/el/6/i686/chef-10.12.0-10.el6.i686.rpm' },
-                                           }
-                                  }
-                        }
-      File.stub!(:read).and_return(JSON.generate(build_list_json))
+    before :each do
+      # Use our dummy data
+      Omnitruck.stub!(:build_list).and_return(client_data("build_list"))
     end
 
     # This should probably return a 400
@@ -68,19 +53,11 @@ describe 'Omnitruck' do
     end
 
     it "should return a rc/beta version if specified" do
-      get '/download', :v => "10.14.10.rc.0-1", :p => "el", :pv => "5", :m => "x86_64"
+      get '/download', :v => "10.14.0.rc.0-1", :p => "el", :pv => "5", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.14.10.rc.0-1.el5.x86_64.rpm'
-    end
-
-    it "should return a specific version if specified, even if it doesn't match a version regex" do
-      get '/download', :v => "10,version-regex-is-fun", :p => "el", :pv => "5", :m => "x86_64"
-      last_response.should be_redirect
-      follow_redirect!
-      http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10,version-regex-is-fun.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.14.0.rc.0-1.el5.x86_64.rpm'
     end
 
     it "should return the latest stable (numeric) version if version is empty string" do
@@ -88,7 +65,7 @@ describe 'Omnitruck' do
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.14.8-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.16.2-1.el5.x86_64.rpm'
     end
 
     it "should return the latest stable (numeric) version if no version is specified" do
@@ -96,7 +73,7 @@ describe 'Omnitruck' do
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.14.8-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.16.2-1.el5.x86_64.rpm'
     end
 
     it "should return the latest prerelease (numeric) version if no version is specified and prerelease is set" do
@@ -104,15 +81,15 @@ describe 'Omnitruck' do
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.14.10.rc.0-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-10.16.0.rc.1-1.el5.x86_64.rpm'
     end
 
     it "should return the latest iteration if no iteration is specified (x.y.z-iteration)" do
-      get '/download', :v => "10.12.0", :p => "el", :pv => "6", :m => "i686"
+      get '/download', :v => "10.14.4", :p => "el", :pv => "6", :m => "i686"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/6/i686/chef-10.12.0-10.el6.i686.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/6/i686/chef-10.14.4-2.el6.i686.rpm'
     end
 
   end
@@ -351,19 +328,9 @@ describe 'Omnitruck' do
   end # Semantic Versioning Support
   
   describe "/download-server" do
-    before do
-      build_server_list_json = { 'run_data' => { 'timestamp' => 'Thu Aug 16 11:48:08 -0700 2012' }, 
-                                 'el' => { '5' => { 'x86_64' => { '11.0.0-1' => '/el/5/x86_64/chef-server-11.0.0-1.el5.x86_64.rpm',
-                                                                  '11.1.0.rc.0-1' => '/el/5/x86_64/chef-server-11.1.0.rc.0-1.el5.x86_64.rpm',
-                                                                  '11.2.0-alpha-1-g123456' => '/el/5/x86_64/chef-server-11.2.0-alpha-1-g123456-1.el5.x86_64.rpm',
-                                                                  '11,version-regex-is-fun' => '/el/5/x86_64/chef-server-11,version-regex-is-fun.el5.x86_64.rpm' }
-                                                  },
-                                           '6' => { 'i686' => { '11.0.0-1' => '/el/6/i686/chef-server-11.0.0-1.el6.i686.rpm',
-                                                                '11.0.0-10' => '/el/6/i686/chef-server-11.0.0-10.el6.i686.rpm' }
-                                                  }
-                                         }
-                               }
-      File.stub!(:read).and_return(JSON.generate(build_server_list_json))
+    before :each do
+      # Use our dummy data
+      Omnitruck.stub!(:build_server_list).and_return(server_data("servers"))
     end
 
     # This should probably return a 400
@@ -389,67 +356,59 @@ describe 'Omnitruck' do
     end
 
     it "should return the version specified" do
-      get '/download-server', :v => "11.0.0-1", :p => "el", :pv => "5", :m => "x86_64"
+      get '/download-server', :v => "10.12.0-1", :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.0.0-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_10.12.0-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return a rc version if specified" do
-      get '/download-server', :v => "11.1.0.rc.0-1", :p => "el", :pv => "5", :m => "x86_64"
+      get '/download-server', :v => "10.14.0.rc.0-1", :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.1.0.rc.0-1.el5.x86_64.rpm'
-    end
-
-    it "should return a specific version if specified, even if it doesn't match a version regex" do
-      get '/download-server', :v => "11,version-regex-is-fun", :p => "el", :pv => "5", :m => "x86_64"
-      last_response.should be_redirect
-      follow_redirect!
-      http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11,version-regex-is-fun.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_10.14.0.rc.0-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return an alpha version if specified" do
-      get '/download-server', :v => "11.2.0-alpha-1-g123456", :p => "el", :pv => "5", :m => "x86_64"
+      get '/download-server', :v => "11.0.0-alpha.1", :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.2.0-alpha-1-g123456-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_11.0.0-alpha.1-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return the latest stable (numeric) version if version is empty string" do
-      get '/download-server', :v => "", :p => "el", :pv => "5", :m => "x86_64"
+      get '/download-server', :v => "", :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.0.0-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_11.0.0-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return the latest stable (numeric) version if no version is specified" do
-      get '/download-server', :p => "el", :pv => "5", :m => "x86_64"
+      get '/download-server', :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.0.0-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_11.0.0-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return the latest prerelease (numeric) version if no version is specified and prerelease is set" do
-      get '/download-server', :p => "el", :pv => "5", :m => "x86_64", :prerelease => "true"
+      get '/download-server', :p => "ubuntu", :pv => "10.04", :m => "x86_64", :prerelease => "true"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/5/x86_64/chef-server-11.2.0-alpha-1-g123456-1.el5.x86_64.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_11.0.0-rc.1-1.ubuntu.10.04_amd64.deb'
     end
 
     it "should return the latest iteration if no iteration is specified (x.y.z-iteration)" do
-      get '/download-server', :v => "11.0.0", :p => "el", :pv => "6", :m => "i686"
+      get '/download-server', :v => "10.14.4", :p => "ubuntu", :pv => "10.04", :m => "x86_64"
       last_response.should be_redirect
       follow_redirect!
       http_type_string = URI.split(last_request.url)[0]
-      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/el/6/i686/chef-server-11.0.0-10.el6.i686.rpm'
+      last_request.url.should == http_type_string + '://opscode-omnitruck-test.s3.amazonaws.com/ubuntu/10.04/x86_64/chef-server_10.14.4-2.ubuntu.10.04_amd64.deb'
     end
 
   end
