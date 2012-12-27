3c10ce620ad42f7275f09f404a7b4f5dfee7536a
Seth Chisamore <schisamo@opscode.com>
2012-12-27 17:21:16 -0500

    Hello there old friend...TRAILING WHITESPACE!!!

--- lib/opscode/version_tester.rb
+++ lib/opscode/version_tester.rb
@@ -11,25 +11,25 @@ module Opscode
 
     CLIENT_URL = "http://www.opscode.com/chef/full_client_list"
     SERVER_URL = "http://www.opscode.com/chef/full_server_list"
-    
+
     def self.current_client_versions
       versions_from_url(CLIENT_URL)
     end
-    
+
     def self.current_server_versions
       versions_from_url(SERVER_URL)
     end
-    
+
     def self.versions_from_url(url)
       data = JSON.parse(`curl --silent #{url}`)
       unique_versions(data)
     end
-    
+
     def self.versions_from_file(file)
       data = JSON.parse(File.read(file))
       unique_versions(data)
     end
-    
+
     def self.unique_versions(data)
       (data.keys.map do |platform|
          versions = data[platform]
@@ -39,15 +39,15 @@ module Opscode
              installers  = data[platform][version][architecture]
              installers.keys
            end
-         end 
+         end
        end).flatten.sort.uniq
     end
-    
+
     def self.test_with_version(version_class)
       version_strings = current_client_versions | current_server_versions
-      
+
       valid, invalid = [], []
-      
+
       version_strings.each do |v|
         begin
           ver = version_class.new(v)
@@ -56,7 +56,7 @@ module Opscode
           invalid << v
         end
       end
-      
+
       puts "Results for #{version_class}"
       puts
       puts "Valid Versions ======================="
@@ -66,7 +66,7 @@ module Opscode
       invalid.each{|v| puts "\t#{v}"}
       puts
     end
-    
+
     def self.show_version_internals(v)
       puts "\t#{v} = #{v.class}"
       puts "\tmajor      = #{v.major}"
@@ -79,15 +79,15 @@ module Opscode
 
     def self.test_all
       version_strings = current_client_versions | current_server_versions
-      
+
       valid, invalid = [], []
-      
+
       # This is horrible, but necessary to sanely handle the current
       # variation we see.
       version_strings.each do |v|
         begin
           if v.start_with?("10.")
-            begin 
+            begin
               ver = Opscode::Version::Rubygems.new(v)
               valid << ver
             rescue
@@ -112,7 +112,7 @@ module Opscode
           invalid << v
         end
       end
-      
+
       puts "Results for All Version Styles"
       puts
       puts "Valid Versions ======================="
