ff1022e4b32d568c7f8ab0ae3a68c2b8b27d7649
tylercloke <tyler@opscode.com>
2013-03-19 16:18:18 -0700

    Refactored s3 parser ruby script into methods for easy logic flow.

--- backup/s3-parse-manifest-json.rb
+++ backup/s3-parse-manifest-json.rb
@@ -11,80 +11,87 @@ require "JSON"
 require "colorize"
 require 'fileutils'
 
-# grab the s3 key from s3cfg
-SECRET_KEY=`grep secret_key ../config/s3cfg | cut -d ' ' -f 3`
-ACCESS_KEY=`grep access_key ../config/s3cfg | cut -d ' ' -f 3`
+def parse_s3_manifest(json, reversed_json, output_root)
+  # for each platform => plat_version => architecture => chef_version
+  json.each                       do |platform, platform_value|
+    platform_value.each             do |platform_version, platform_version_value| 
+      platform_version_value.each     do |arch, arch_value|
+        arch_value.each                 do |chef_version, chef_version_value|
+          # get the name of the omnibus version dir
+          omnibus_dir = reversed_json[platform][platform_version][arch]
+          # get the output path in omnibus format (see readme if interested in format)
+          output_dir = "#{output_root}/#{chef_version}/#{omnibus_dir}/pkg/"
 
-s3 = UberS3.new({
-  :access_key         => ACCESS_KEY.strip,
-  :secret_access_key  => SECRET_KEY.strip,
-  :bucket             => ARGV[1],
-  :adapter            => :net_http
-})
+          # get the name of the chef build file
+          file_name = chef_version_value.split("/")[-1]
+          
+          # make output path and download file if output path doesn't exist
+          if File.directory? output_dir
+            puts  "Directory already exists, not downloading #{file_name}".yellow
+            `echo "Directory already exists, not downloading #{file_name}" >> backup-error-log`
+          else
+            # make directory
+            FileUtils.mkdir_p(output_dir)
 
-# file where each line is a ruby release version
-file = File.open(ARGV[0]).read
-# inverse of omnibus-chef/jenkins/release.rb file
-reversed_json = JSON.parse(File.open(ARGV[4]).read)
-
-# for each release manifest
-file.each_line do |line|
-  parts = line.split('/')
-  # get the release manifest file name
-  filename = "#{ARGV[2]}/#{parts[parts.length-1]}".strip
-
-  file_found = false
-  filename = filename.gsub(/\+/, "%2B") 
-  begin
-    # get the json from the current release manifest
-    json = JSON.parse(s3.get(filename).value)
-    file_found = true
-  rescue
-    puts  "COULD NOT OPEN S3 FILE #{filename}".red
-    `echo "COULD NOT OPEN S3 FILE #{filename}" >> backup-error-log`
-  end
-
-  if file_found
-    puts "Downloading all files from #{filename}".green
-    # for each platform => plat_version => architecture => chef_version
-    json.each do |platform, platform_value|
-      platform_value.each do |platform_version, platform_version_value| 
-        platform_version_value.each do |arch, arch_value|
-          arch_value.each do |chef_version, chef_version_value|
-            # get the name of the omnibus version dir
-            omnibus_dir = reversed_json[platform][platform_version][arch]
-            # get the output path in omnibus format (see readme if interested in format)
-            output_dir = "#{ARGV[3]}/#{chef_version}/#{omnibus_dir}/pkg/"
-
-            # get the name of the chef build file
-            file_name = chef_version_value.split("/")[-1]
-            
-            # make output path if it doesn't exist
-            directory_exists = false
-            begin
-              if File.directory? output_dir
-                directory_exists = true
-                raise
-              end
-              FileUtils.mkdir_p(output_dir)
-            rescue
-              puts  "Directory already exists, not downloading #{file_name}".yellow
-              `echo "Directory already exists, not downloading #{file_name}" >> backup-error-log`
-              break
-            end
-            
-            # rip that sucker down from s3 and into your output path, if you haven't dl-ed it yet
-            if not directory_exists 
-              begin
-                `wget https://opscode-omnitruck-release.s3.amazonaws.com#{chef_version_value.gsub(/\+/, "%2B")} -O #{output_dir}/#{file_name}`
-              rescue
-                puts  "Failed to download https://opscode-omnitruck-release.s3.amazonaws.com#{chef_version_value.gsub(/\+/, "%2B")} to file #{file_name}".red
-                `echo "Failed to download https://opscode-omnitruck-release.s3.amazonaws.com#{chef_version_value.gsub(/\+/, "%2B")} to file #{file_name}" >> backup-error-log`
-              end
-            end
+            # download file from s3 into directory
+            s3_file = chef_version_value.gsub(/\+/, "%2B")
+            output_file = "#{output_dir}/#{file_name}"
+            download_build_from_s3(s3_file, output_file)             
           end
         end
       end
-    end        
+    end
   end
 end
+
+def download_build_from_s3(s3_file, output_file)
+  # rip that sucker down from s3 and into your output path, if you haven't dl-ed it yet
+  begin
+    `wget https://opscode-omnitruck-release.s3.amazonaws.com#{s3_file} -O #{output_file}`
+  rescue
+    puts  "Failed to download https://opscode-omnitruck-release.s3.amazonaws.com#{s3_file} to file #{output_file}".red
+    `echo "Failed to download https://opscode-omnitruck-release.s3.amazonaws.com#{s3_file} to file #{output_file}" >> backup-error-log`
+  end
+end
+
+if __FILE__ == $0
+  # grab the s3 key from s3cfg
+  SECRET_KEY=`grep secret_key ../config/s3cfg | cut -d ' ' -f 3`
+  ACCESS_KEY=`grep access_key ../config/s3cfg | cut -d ' ' -f 3`
+
+  s3 = UberS3.new({
+                    :access_key         => ACCESS_KEY.strip,
+                    :secret_access_key  => SECRET_KEY.strip,
+                    :bucket             => ARGV[1],
+                    :adapter            => :net_http
+                  })
+
+  # file where each line is a ruby release version
+  file = File.open(ARGV[0]).read
+  # inverse of omnibus-chef/jenkins/release.rb file
+  reversed_json = JSON.parse(File.open(ARGV[4]).read)
+
+  # for each release manifest
+  file.each_line do |line|
+    parts = line.split('/')
+    # get the release manifest file name
+    filename = "#{ARGV[2]}/#{parts[parts.length-1]}".strip
+
+    file_found = false
+    filename = filename.gsub(/\+/, "%2B") 
+    begin
+      # get the json from the current release manifest
+      json = JSON.parse(s3.get(filename).value)
+      file_found = true
+    rescue
+      puts  "COULD NOT OPEN S3 FILE #{filename}".red
+      `echo "COULD NOT OPEN S3 FILE #{filename}" >> backup-error-log`
+    end
+
+    if file_found
+      puts "Downloading all files from #{filename}".green
+      parse_s3_manifest(json, reversed_json, ARGV[3])
+    end
+  end
+end
+
