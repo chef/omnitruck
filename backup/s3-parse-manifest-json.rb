965dd0ab9092724209786664c0b763d6d780b0bc
Lamont Granquist <lamont@scriptkiddie.org>
2013-11-11 15:13:59 -0800

    add license to all the ruby things

--- backup/s3-parse-manifest-json.rb
+++ backup/s3-parse-manifest-json.rb
@@ -1,3 +1,24 @@
+#--
+# Author:: Tyler Cloke (tyler@opscode.com)
+# Author:: Stephen Delano (stephen@opscode.com)
+# Author:: Seth Chisamore (sethc@opscode.com)
+# Author:: Lamont Granquist (lamont@opscode.com)
+# Copyright:: Copyright (c) 2010-2013 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Author: Tyler Cloke <tyler@opscode.com>
 #
 # Usage: ruby s3-parse-manifest-json.rb s3-filenames-file s3-bucket-name s3-sub-bucket-name output-dir reversed-ruby (see reverse-release-json.rb for last arg help)
