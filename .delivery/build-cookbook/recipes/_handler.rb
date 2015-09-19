c7eb22fcaa90c38b4fbfce5f87a01a2caee69141
Christopher Webber <cwebber@getchef.com>
2015-09-19 07:29:11 -0700

    Notify the #operations channel when we fail in the URD

--- .delivery/build-cookbook/recipes/_handler.rb
+++ .delivery/build-cookbook/recipes/_handler.rb
@@ -4,11 +4,17 @@ Chef_Delivery::ClientHelper.enter_client_mode_as_delivery
 
 slack_creds = encrypted_data_bag_item_for_environment('cia-creds','slack')
 
+if ['union', 'rehearsal', 'delivered'].include?(node['delivery']['change']['stage'])
+  slack_channels = slack_creds['channels'].push('#operations')
+else
+  slack_channels = slack_creds['channels']
+end
+
 chef_handler "BuildCookbook::SlackHandler" do
   source File.join(node["chef_handler"]["handler_path"], 'slack.rb')
   arguments [
     :webhook_url => slack_creds['webhook_url'],
-    :channels  => slack_creds['channels'],
+    :channels  => slack_channels,
     :username => slack_creds['username']
   ]
   supports :exception => true
