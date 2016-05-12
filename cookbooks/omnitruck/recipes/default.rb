a1853c05df8f9a64305addc2dd475b4dc0493b3c
Christopher Webber <cwebber@chef.io>
2016-05-12 14:45:07 -0700

    Standardizes ruby install
    This brings the omnitruck repo inline with the other projects that have
    standardized on the same ruby install.
    
    Signed-off-by: Christopher Webber <cwebber@chef.io>

--- cookbooks/omnitruck/recipes/default.rb
+++ cookbooks/omnitruck/recipes/default.rb
@@ -5,6 +5,6 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'omnitruck::_users'
-include_recipe 'omnitruck::_ruby'
+include_recipe 'cia_infra::ruby'
 include_recipe 'omnitruck::_nginx'
 include_recipe 'omnitruck::_application'
