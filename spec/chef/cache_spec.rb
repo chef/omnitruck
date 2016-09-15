7fd665dcf5c5c48bb9ea4633206e496734c933d8
Patrick Wright <patrick@chef.io>
2016-09-15 14:06:20 -0700

    requests for automate map to delivery

--- spec/chef/cache_spec.rb
+++ spec/chef/cache_spec.rb
@@ -5,7 +5,7 @@ describe Chef::Cache do
   let(:cache_path) { Dir.mktmpdir }
   after { FileUtils.rm_rf(cache_path) }
 
-  it "can update the cache from bintray" do
+  it "can update the cache from package router" do
     Chef::Cache.new(cache_path).update
 
     Chef::Cache::KNOWN_CHANNELS.each do |channel|
@@ -17,18 +17,4 @@ describe Chef::Cache do
       end
     end
   end
-
-  it "can update the cache from artifactory", :focus do
-    Chef::Cache.new(cache_path, true).update
-
-    Chef::Cache::KNOWN_CHANNELS.each do |channel|
-      Chef::Cache::KNOWN_PROJECTS.each do |project|
-        manifest_path = File.join(cache_path, channel, "#{project}-manifest.json")
-        expect(File.exist?(manifest_path)).to be(true)
-
-        expect(JSON.parse(File.read(manifest_path))["run_data"]["timestamp"]).to be_a(String)
-      end
-    end
-  end
-
 end
