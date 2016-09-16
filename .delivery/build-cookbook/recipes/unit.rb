9a9a58a60b00f5487b1fd1916067e1be836b1ad2
Patrick Wright <patrick@chef.io>
2016-09-15 18:05:57 -0700

    use vendored rake exec

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -10,7 +10,7 @@ include_recipe 'delivery-truck::unit'
 include_recipe 'cia_infra::bundler_install_deps'
 
 execute 'run rspec' do
-  command "bundle exec rake spec"
+  command "bin/rake spec"
   cwd node['delivery_builder']['repo']
   environment({
     "PATH" => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games'
