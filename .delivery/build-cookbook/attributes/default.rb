55d718bd60a81c48959d9518664ac867a20e88a5
jtimberman <joshua@chef.io>
2017-03-23 13:16:41 -0600

    specify the security group via attribute
    For some reason the CIAInfra helper isn't picking up the security
    group, so let's set it in an attribute.
    
    Signed-off-by: Joshua Timberman <joshua@chef.io>

--- .delivery/build-cookbook/attributes/default.rb
+++ .delivery/build-cookbook/attributes/default.rb
@@ -1,3 +1,4 @@
 # Change the default AMI to Ubuntu 16.04 so we can properly use
 # systemd for habitat's services.
 default['ciainfra']['image_id'] = 'ami-7ac6491a'
+default['ciainfra']['security_groups'] = ['sg-9d9223f9']
