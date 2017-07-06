8f64d444d662c33f7d09375194c01a228df4b6ad
Patrick Wright <patrick@chef.io>
2017-07-06 11:58:50 -0700

    remove bundler-audit check
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- .delivery/build-cookbook/recipes/security.rb
+++ .delivery/build-cookbook/recipes/security.rb
@@ -3,14 +3,3 @@
 #
 # This recipe is run as the delivery user
 ################################################################################
-
-include_recipe 'cia_infra::bundler_install_deps'
-
-execute 'bundler-audit' do
-  command "bundle exec bundle-audit update && bundle exec bundle-audit check"
-  cwd node['delivery_builder']['repo']
-  environment({
-    "PATH" => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games'
-  })
-  user node['delivery_builder']['build_user']
-end
