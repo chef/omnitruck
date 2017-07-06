2b9f49e206219eee40808e199305af5ad2e06223
Patrick Wright <patrick@chef.io>
2017-07-06 08:50:32 -0700

    update build phase and travis config
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- .delivery/build-cookbook/recipes/lint.rb
+++ .delivery/build-cookbook/recipes/lint.rb
@@ -7,14 +7,3 @@
 # Run lint against the cookbooks
 include_recipe 'delivery-truck::lint'
 include_recipe 'habitat-build::lint'
-include_recipe 'cia_infra::bundler_install_deps'
-
-#TODO rubocop all the things!
-#execute 'lint' do
-#  command "rubocop"
-#  cwd node['delivery_builder']['repo']
-#  environment({
-#    "PATH" => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games'
-#  })
-#  user node['delivery_builder']['build_user']
-#end
