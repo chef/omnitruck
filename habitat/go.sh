70978a24ff95939b20228c5d4649b41fe50a79c7
Ryan Hass <rhass@users.noreply.github.com>
2017-07-24 14:30:09 -0700

    Rename `omnitruck-unicorn-proxy` for clarity.
    The `omnitruck-unicorn-proxy` hab package has been renamed to
    `omnitruck-web-proxy` to attempt make it more clear as to what
    application in the stack is being proxied.
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

--- habitat/go.sh
+++ habitat/go.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 build && \
-build habitat/omnitruck-unicorn-proxy && \
+build habitat/omnitruck-web-proxy && \
 build habitat/omnitruck-poller && \
 build habitat/omnitruck-web && \
 hab start $HAB_ORIGIN/omnitruck && \
-hab start $HAB_ORIGIN/omnitruck-unicorn-proxy && \
+hab start $HAB_ORIGIN/omnitruck-web-proxy && \
 hab start $HAB_ORIGIN/omnitruck-poller && \
 hab start $HAB_ORIGIN/omnitruck-web
