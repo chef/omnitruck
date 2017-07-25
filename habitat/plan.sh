19413f4f0be8fe5bcb1eff68bdf6255961f668e7
Patrick Wright <patrick@chef.io>
2017-07-25 14:20:56 -0700

    Set SRC_PATH in omnitruck plan.sh
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -7,5 +7,10 @@ pkg_version="0.1.0"
 pkg_maintainer="Chef Engineering Services <eng-services@chef.io>"
 pkg_license=('Apache-2.0')
 
+# This is set to force the source path to the root level during automate builds
+# You must build plan from the root dir with command `build habitat`
+# Recommended to execute `scripts/hab-it`
+SRC_PATH="../"
+
 pkg_scaffolding=core/scaffolding-ruby
 scaffolding_ruby_pkg=core/ruby/$(cat "$PLAN_CONTEXT/../.ruby-version")
