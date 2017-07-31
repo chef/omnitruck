8dec347c3fae30b85b78c453f1ba51e72ed042e5
Ryan Hass <rhass@users.noreply.github.com>
2017-07-31 14:06:38 -0700

    Fix Omnitruck hab versioning
    This ensures packages which depend on the latest version of the habitat
    omnitruck package actually install the correct version for "latest."
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -3,10 +3,17 @@
 #
 pkg_name=omnitruck
 pkg_origin=chef-es
-pkg_version="0.1.0"
+# Previous versions of omnitruck used an automatic version assignment for the
+# habitat pkg_version value which was base on the git commit count. Since we
+# use `sort --version-sort -r` in plan-build to determine the latest version of
+# any given package we need to set a value which sorts higher than the 950+
+# commits which are in the repo at the time of this change. Since we have 
+# additional hab services which depend on the omnitruck hab package, they will
+# uninintentionally install a version which is not the latest unless we use a
+# version which starts with 1000.x.x or greater.
+pkg_version=1000.0.0
 pkg_maintainer="Chef Engineering Services <eng-services@chef.io>"
 pkg_license=('Apache-2.0')
-
 # This is set to force the source path to the root level during automate builds
 # You must build plan from the root dir with command `build habitat`
 # Recommended to execute `scripts/hab-it`
