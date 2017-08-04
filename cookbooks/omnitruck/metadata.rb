79a39fb0aa89ab5735903c2209b97eb59f16603d
Patrick Wright <patrick@chef.io>
2017-08-04 07:15:04 -0700

    stop hab-sup service
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- cookbooks/omnitruck/metadata.rb
+++ cookbooks/omnitruck/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache2'
 description 'Installs/Configures omnitruck'
 long_description 'Installs/Configures omnitruck'
-version '0.4.20'
+version '0.4.21'
 
 depends 'delivery-sugar'
 depends 'cia_infra'
--- cookbooks/omnitruck/recipes/stop_services.rb
+++ cookbooks/omnitruck/recipes/stop_services.rb
@@ -3,18 +3,6 @@
 # Recipe:: stop_services
 #
 
-hab_service 'chef-es/omnitruck-app' do
-  action :stop
-end
-
-hab_service 'chef-es/omnitruck-poller' do
-  action :stop
-end
-
-hab_service 'chef-es/omnitruck-web' do
-  action :stop
-end
-
-hab_service 'chef-es/omnitruck-web-proxy' do
+service 'hab-sup-default' do
   action :stop
 end
