5bd6e1cac96392639ec98eb0ba9360bf18f06523
Patrick Wright <patrick@chef.io>
2017-11-09 11:19:28 -0700

    update hab package as standalone
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- habitat/omnitruck-web/plan.sh
+++ habitat/omnitruck-web/plan.sh
@@ -3,15 +3,14 @@ pkg_origin=chef-es
 pkg_version="0.1.0"
 pkg_maintainer="Chef Engineering Services <eng-services@chef.io>"
 pkg_license=('Apache-2.0')
-
 pkg_deps=(
-  $HAB_ORIGIN/omnitruck-app
+  core/coreutils
 )
 
-do_build() {
-  return 0
-}
+pkg_scaffolding=core/scaffolding-ruby
+scaffolding_ruby_pkg=core/ruby/$(cat "$SRC_PATH/../../.ruby-version")
 
 do_install() {
-  return 0
+  do_default_install
+  fix_interpreter "$pkg_prefix/app/poller" core/coreutils bin/env
 }
