82d3d39b34818f165907f96455cf669ede5be256
Patrick Wright <patrick@chef.io>
2017-11-13 08:51:05 -0700

    remove plan steps from web and poller
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- habitat/omnitruck-poller/plan.sh
+++ habitat/omnitruck-poller/plan.sh
@@ -3,15 +3,14 @@ pkg_origin=chef-es
 pkg_version="0.1.0"
 pkg_maintainer="Chef Engineering Services <eng-services@chef.io>"
 pkg_license=('Apache-2.0')
-pkg_deps=(
-  core/coreutils
-)
 
 pkg_scaffolding=core/scaffolding-ruby
-echo $SRC_PATHbuild
 scaffolding_ruby_pkg=core/ruby/$(cat "$SRC_PATH/../../.ruby-version")
 
-do_install() {
-  do_default_install
-  fix_interpreter "$pkg_prefix/app/poller" core/coreutils bin/env
-}
+# do_build() {
+#   return 0
+# }
+
+# do_install() {
+#   return 0
+# }
