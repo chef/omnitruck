e55544ce3861a133decfa99715450b10391ac787
Nathaniel Kierpiec <nkierpiec@chef.io>
2021-08-17 16:06:32 -0400

    add to update version for Helm chart version value
    Signed-off-by: Nathaniel Kierpiec <nkierpiec@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -1,8 +1,9 @@
-
 #!/bin/sh
+
 set -evx
 
 version=$(cat VERSION)
 
 # Update version for dobi
 sed -i -r "s/^(\\s*)VERSION: \".+\"/\\1VERSION: \"$version\"/" .expeditor/build.docker.yml
+sed -i.bak -r "s/^version: .+/version: $version/" charts/omnitruck/Chart.yaml
