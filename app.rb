51a6047bbd3ce3449c1ec109f59f5d7ad650a62f
Chef Expeditor <chef-ci@chef.io>
2024-09-18 17:15:37 +0000

    Bump version to 0.1.36 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- app.rb
+++ app.rb
@@ -342,7 +342,7 @@ class Omnitruck < Sinatra::Base
   end
 
   get '/_version' do
-    JSON.pretty_generate(:version => "0.1.35")
+    JSON.pretty_generate(:version => "0.1.36")
   end
 
   get '/products' do
